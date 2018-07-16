21686d6ccd0be82ae7dfd5d294bab8063f14af1c
DJ_Kovrik <djkovrik@gmail.com>
2018-07-16 20:08:19 +0300

    Riddle 6 solved

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle6.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle6.kt
@@ -1,6 +1,7 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.Single
+import io.reactivex.functions.BiFunction
 
 object Riddle6 {
   /**
@@ -8,7 +9,6 @@ object Riddle6 {
    *
    * Use case: Execute two network requests in parallel and wait for each other and process the combined data.
    */
-  fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> {
-    TODO()
-  }
+  fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> =
+          Single.zip(first, second, BiFunction { t1, t2 -> Pair(t1, t2) })
 }
